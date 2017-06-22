ef77ddff0fe7a75d4b7d32cb9f8d5c325cf4eb98
kumpakri <kumpakri@gmail.com>
2017-06-22 17:15:09 +0200

    bluetooth_test.c modified to work with new commits.

--- siemens_car/src/bluetooth_test.c
+++ siemens_car/src/bluetooth_test.c
@@ -14,13 +14,14 @@
 #include <stdio.h>
 
 int main(void) {
+	init_motors();
 	configurePinsPA2andPA3();
 	initUART2(9600);
 	LEDinit();
 
 	char cmdLeftArray[NUM_OF_QUANTILES] = {'a','b','c','d','e','f','g','h','i','j','k','B','C','D','E','F','G','H','I','J','K'};
 	char cmdRightArray[NUM_OF_QUANTILES] = {'l','m','n','o','p','q','r','s','t','u','v','M','N','O','P','Q','R','S','T','U','V'};
-	int cmdValues[NUM_OF_QUANTILES] = {0,-10,-20,-30,-40,-50,-60,-70,-80,-90,-100,10,20,30,40,50,60,70,80,90,100};
+	int cmdValues[NUM_OF_QUANTILES] = {0,-40,-43,-46,-49,-52,-55,-58,-61,-64,-67,40,43,46,49,52,55,58,61,64,67};
 	volatile int number;
 
 	char *ptr = "hello\r\n";
@@ -32,7 +33,7 @@ int main(void) {
 	while(1){
 
 		int i = 0;
-		while(i<100000){i++;}
+		while(i<1000){i++;}
 		if (  isNewMessage() ) {
 			cmd = getCommand();
 
@@ -40,30 +41,30 @@ int main(void) {
 			for(i=0;i<NUM_OF_QUANTILES;i++){
 				/* Left motor */
 				if(cmd == cmdLeftArray[i]) {
-					set_speed_motor1(cmdValues[i]);
+					set_speed_motorLeft(cmdValues[i]);
 				}
 				/* Right motor */
 				if(cmd == cmdRightArray[i]) {
-					set_speed_motor2(cmdValues[i]);
+					set_speed_motorRight(cmdValues[i]);
 				}
 			}
 
 			/* STOP command */
 			if(cmd == 'x') {
-				set_speed_motor1(0);
-				set_speed_motor2(0);
+				set_speed_motorLeft(0);
+				set_speed_motorRight(0);
 			}
 
 
 			setNewMessage(0);
 		}
-
+/*
 		char str[] = "L:%i\r\n";
 		char str2[12];
 		number = get_speed_motor1();
 		sprintf(str2,str,number);
 		USART_puts(USART2,str2);
-
+*/
 	}
 }
 
